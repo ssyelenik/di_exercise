457d2dd91d700bc854a22ca1bba6186c66e07700
Sharon Yelenik <ssyelenik@gmail.com>
2020-05-07 19:58:44 +0300

    Week 3 Day 2 Exercise

---
+++ week_3/day_2/ex_1.py/script.js
@@ -0,0 +1,12 @@
+let newDog="Chihuahua";
+console.log(newDog);
+let newDogLength=newDog.length;
+console.log(newDogLength);
+console.log(newDog.toUpperCase());
+console.log(newDog.toLowerCase());
+if (newDog="Chihuahua"){
+	console.log("I LOVE Chihuahua, it's my favorite dog");
+}
+else{
+	console.log("I don't care, I prefer CATS");
+}
