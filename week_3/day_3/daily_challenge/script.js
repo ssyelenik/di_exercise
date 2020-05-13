89a84237f07e06f6ad4d331cf2d248e795684cc1
Sharon Yelenik <ssyelenik@gmail.com>
2020-05-13 17:29:51 +0300

    Correction on Week 3 Day 3 Daily Challenge

--- week_3/day_3/daily_challenge/script.js
+++ week_3/day_3/daily_challenge/script.js
@@ -1,22 +1,27 @@
 var word=[];
 for (let i=0; i<4; i++){
-	word.push(prompt("Enter a number: "));
+	word.push(prompt("Enter a word: "));
 }
 
 for (i=0; i<word.length; i++){
+	console.log(word[i]);
 	console.log(typeof word[i]);
+	console.log(isNaN(word[i]));
 	if (isNaN(word[i])){
 			}
 	else{
 		word.splice(i,1);
 	}
 }
-let numReps=0;
+/*let numReps=0;*/
 let letterRepetition=0;
-let wordStatus=[];
-for (let i=0; i<word.length; i++){
-	console.log("here: " + word[i]);	
-	numReps=0;
+let temp="";
+numReps=0;
+/*let wordStatus=[];*/
+let longestWord="";
+for (let i=0; i<word.length; i++){	
+	
+	temp=word[i];
 	for (let x=0; x<word[i].length; x++){
 		letterRepetition=0;
 		for (let z=0; z<word[i].length; z++){
@@ -26,19 +31,21 @@ for (let i=0; i<word.length; i++){
 			if (word[i][x]===word[i][z]){
 			letterRepetition++;
 			}
+		}
 		if (numReps<letterRepetition){
 			numReps=letterRepetition;
-		}
+			longestWord=word[i];
 		}
 	}
+}
 	
-	wordStatus.splice(i,0,numReps);
-	console.log(wordStatus[i]);
+/*	wordStatus.splice(i,0,numReps);
 }
 
 for(let x of wordStatus){
-	console.log(x);
+	console.log("wordstatus" +x);
 }
+
 let hold=0
 let winner=0;
 for (i=0; i<wordStatus.length; i++){
@@ -47,5 +54,5 @@ for (i=0; i<wordStatus.length; i++){
 		hold=wordStatus[i];
 	}
 
-}
-console.log("The word with the most repeating letters is: " +word[winner]);
+}*/
+console.log("The word with the most repeating letters is: " + longestWord);
