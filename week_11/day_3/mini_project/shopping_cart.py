a2b58c54899d069fb34fc11af32712fcefd6611b
ssyelenik <ssyelenik@gmail.com>
2020-07-08 00:05:21 +0300

    Week 11 Day 3 Miniproject Fixes

--- week_11/day_3/mini_project/shopping_cart.py
+++ week_11/day_3/mini_project/shopping_cart.py
@@ -11,46 +11,70 @@ print(test_dict)
 
 @shopping.route("/homepage", methods=['GET','POST'])
 def homepage():
-    if f.request.method=="POST":
-        search_items=[]
-        button=f.request.form
-        search_word=f.request.form['search_item']
-        if "search_item" in button:
-            available_products=scb.get_available_products()
-            for prod in available_products:
-                for key in prod:
-                    if search_word in key or search_word in prod[key]:
-                        search_items.append(prod)
-            if search_items=="":
-                pass
-            else:
-                num=len(search_items)
-                print("HERE")
-                return f.render_template('home.bin/view_search_products.html',search_products=search_items, num=num)
+##    if f.request.method=="POST":
+##        search_items=[]
+##        button=f.request.form
+##        search_word=f.request.form['search_item']
+##        if "search_item" in button:
+##            available_products=scb.get_available_products()
+##            for prod in available_products:
+##                for key in prod:
+##                    if search_word in key or search_word in prod[key]:
+##                        search_items.append(prod)
+##            if search_items=="":
+##                pass
+##            else:
+##                num=len(search_items)
+##                print("HERE")
+##                return f.render_template('home.bin/view_search_products.html',search_products=search_items, num=num)
     return f.render_template("home.bin/homepage.html")
 
 @shopping.route("/view_search_products", methods=['GET','POST'])
-def view_search_products(search_products,num):
-    print("MIRACLE HERE",search_products,num)
-    num=len(search_products)
-    print("***WEIRD HERE1***")
+def view_search_products():
+    global search_items
+    global num
+    global buy_products
     if f.request.method=="POST":
-        print("***WEIRD HERE2***")
-        button = f.request.form
-        if "view_product" in button:
-            view_product=f.request.form['view_product'] 
-            return f.render_template("home.bin/view_single_product.html",product=search_products[int(view_product)])
-        elif "buy_product" in button:
+        
+        button = f.request.form.to_dict()
+        print(button)
+        print(type(button))
 
-            buy_product=f.request.form['buy_product']
-            try:
-                buy_products.append(search_products[int(buy_product)])
-            except:
-                buy_product=ast.literal_eval(buy_product)
-                buy_products.append(buy_product)
-    print("NOW IT SHOULD OPEN IN VIEW SEARCH")
-    print(search_products)
-    return f.render_template('view_search_products',search_products=search_products, num=num)
+        for keys in button:
+            print("FINDING KEYS IN FORM")
+
+            if keys=="search_item":
+                search_items=[]
+                search_word=f.request.form['search_item']
+                available_products=scb.get_available_products()
+                for prod in available_products:
+                    for key in prod:
+                        if search_word in key or search_word in prod[key]:
+                            search_items.append(prod)
+                if search_items=="":
+                    return f.render_template("home.bin/homepage.html")
+                else:
+                    num=len(search_items)
+        
+            if keys=="view_product":
+                print("VIEW PRODUCT BUTTON PRESSED")
+                view_product=f.request.form['view_product'] 
+                return f.render_template("home.bin/view_single_product.html",product=search_items[int(view_product)])
+
+            elif keys=="buy_product":
+                print("BUY PRODUCT BUTTON PRESSED")
+                buy_product=f.request.form['buy_product']
+                print("BUY PRODUCT BUTTON VAL",buy_product)
+                
+                try:
+                    if search_items[int(buy_product)] not in buy_products:
+                        buy_products.append(search_items[int(buy_product)])
+                        print(buy_products)
+                except:
+                    buy_product=ast.literal_eval(buy_product)
+                    if buy_product not in buy_products:
+                        buy_products.append(buy_product)
+    return f.render_template('home.bin/view_search_products.html',search_products=search_items, num=num)
 
 @shopping.route("/view_all_products", methods=['GET','POST'])
 def view_all_products():
@@ -67,11 +91,14 @@ def view_all_products():
             return f.render_template("home.bin/view_single_product.html",product=available_products[int(view_product)])
         elif "buy_product" in button:
             buy_product=f.request.form['buy_product']
+            
             try:
-                buy_products.append(available_products[int(buy_product)])
+                if available_products[int(buy_product)] not in buy_products:
+                    buy_products.append(available_products[int(buy_product)])
             except:
                 buy_product=ast.literal_eval(buy_product)
-                buy_products.append(buy_product)
+                if buy_product not in buy_products:
+                    buy_products.append(buy_product)
         
     return f.render_template("home.bin/view_all_products.html",available_products=available_products, num=num)
 
@@ -79,11 +106,12 @@ def view_all_products():
 def view_single_product(view_product):
     global buy_products
     if f.request.method=="POST":
+        print(button)
         button = f.request.form
         if 'product_to_buy' in button:
-            
-            product_to_buy=f.request.form['product_to_buy']
-            buy_products.append(buy_product)
+            if buy_product not in buy_products:
+                product_to_buy=f.request.form['product_to_buy']
+                buy_products.append(buy_product)
             
     return f.render_template("home.bin/view_single_product.html",product=view_product)
 
